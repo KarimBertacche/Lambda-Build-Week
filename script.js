945489eb79d6f3bcca31b94920a59cfc44cd2a22
Karim B <mattiakarim@live.it>
2019-05-21 11:06:24 +0200

    Add some logic to the quotes carousel

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Navigation bar scrolling functionality
 window.addEventListener('scroll', event => {
     const navBar = document.querySelector('.navigation');
     if(event.currentTarget.scrollY > 1332) { 
@@ -7,6 +8,107 @@ window.addEventListener('scroll', event => {
     }
 });
 
+//Quotes slide carousel functionality
+const quotes = document.querySelectorAll('.quote-card');
+const backArrow = document.querySelector('.back-arrow');
+const forwardArrow = document.querySelector('.forward-arrow');
+let counter = 0;
+let quoteIdx;
+let quoteLength = quotes.length;
+let prevCount;
+
+quotes.forEach((quote, index) => {
+    quoteIdx = quote.dataset.index = index;
+})
+
+
+backArrow.addEventListener('click', () => {
+    if(counter === 0) 
+    {
+        //Hold previous counter
+        prevCount = counter;
+        //Update the counter
+        counter = quoteLength - 1;
+    } 
+    else 
+    {
+        //Hold previous counter
+        prevCount = counter;
+        //Update the counter
+        --counter;  
+    }
+
+    //Remove Active class & prev/next
+    removeActive();
+    addActive();
+    addPrev(prevCount);
+    addNext(prevCount);
+})
+
+forwardArrow.addEventListener('click', () => {
+    if(counter === quoteLength -1) {
+        prevCount = counter;
+        counter = 0;
+    } else {
+        prevCount = counter;
+        ++counter;
+    }
+    removeActive();
+    addActive();
+    addPrev(prevCount);
+    addNext(prevCount);
+})
+
+const addPrev = (prevCount) => {
+    if(counter === 0){
+        let prevQuote = document.querySelector(`.quote-card[data-index="${quoteLength - 1}`);
+        prevQuote.className += " prev";   
+    } else if(counter === quoteLength -1) {
+        let prevQuote = document.querySelector(`.quote-card[data-index="${counter -1}`);
+        prevQuote.className += " prev";   
+    } else if(counter < prevCount){
+        let prevQuote = document.querySelector(`.quote-card[data-index="${counter - 1}`);
+        prevQuote.className += " prev";  
+    } else if(counter > prevCount) {
+        let prevQuote = document.querySelector(`.quote-card[data-index="${prevCount}`);
+        prevQuote.className += " prev";  
+    }
+} 
+
+const addNext = (prevCount) => {
+    if(counter === quoteLength - 1){
+        let prevQuote = document.querySelector(`.quote-card[data-index="0"]`);
+        prevQuote.className += " next";   
+    } else if(counter === 0) {
+        let prevQuote = document.querySelector(`.quote-card[data-index="${counter + 1}`);
+        prevQuote.className += " next";   
+    } else if(counter > prevCount){
+        let prevQuote = document.querySelector(`.quote-card[data-index="${counter + 1}`);
+        prevQuote.className += " next";  
+    } else if(counter < prevCount) {
+        let prevQuote = document.querySelector(`.quote-card[data-index="${prevCount}`);
+        prevQuote.className += " next";  
+    }
+}
+
+const removeActive = () => {
+    quotes.forEach(quote => {
+        quote.className = "quote-card";
+    })
+}
+
+// const addNext = () => {
+//     if(counter === 0)
+// }
+
+const addActive = () => {
+    let newActive = document.querySelector(`.quote-card[data-index="${counter}"]`);
+    newActive.classList.add('active');
+
+}
+
+
+//Book section drag & drop functionality
 const books = document.querySelectorAll('.book-box');
 let curBook;
 
@@ -40,3 +142,6 @@ dropZone.addEventListener('dragleave', (event) => {
 dropZone.addEventListener('drop', (event) => {
     event.target.append(curBook);
 })
+
+
+
